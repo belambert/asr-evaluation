e94e852d8c40ab05282acee73133f210a90f8d73
Ben Lambert <ben@benjaminlambert.com>
2016-07-10 00:03:25 -0400

    Minor cleanup of setup.py file.

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email='ben@benjaminlambert.com',
     packages=['asr_evaluation'],
     license='LICENSE.txt',
-    description='Evaluating ASR (automatic speech recognitiuon) hypotheses, i.e. computing word error rate.',
-    long_description=open('README.md').read(),
-    install_requires=['edit_distance'],
+    description='Evaluating ASR (automatic speech recognition) hypotheses, i.e. computing word error rate.',
+    install_requires=['edit_distance']
 )
