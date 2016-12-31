5009b231599e4b37dea9f5b2b5eb3225c4c0560b
Ben Lambert <ben@benjaminlambert.com>
2016-12-31 01:27:09 -0500

    Auto-deploy and install evaluate.py.

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name='asr_evaluation',
-    version='0.1.1',
+    version='0.2.0',
     author='Ben Lambert',
     author_email='ben@benjaminlambert.com',
     packages=['asr_evaluation'],
     license='LICENSE.txt',
     description='Evaluating ASR (automatic speech recognition) hypotheses, i.e. computing word error rate.',
-    install_requires=['edit_distance']
+    install_requires=['edit_distance'],
+    scripts=['bin/evaluate.py']
 )
