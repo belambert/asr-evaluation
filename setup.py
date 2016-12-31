fe1124dd6e91d921af8332c43ac6c3032e925d12
Ben Lambert <ben@benjaminlambert.com>
2016-12-31 02:03:53 -0500

    Adding matplotlib dependency to setup.py.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='asr_evaluation',
-    version='0.2.2',
+    version='0.2.3',
     author='Ben Lambert',
     author_email='ben@benjaminlambert.com',
     packages=['asr_evaluation'],
     license='LICENSE.txt',
     description='Evaluating ASR (automatic speech recognition) hypotheses, i.e. computing word error rate.',
-    install_requires=['edit_distance', 'termcolor'],
+    install_requires=['edit_distance', 'termcolor', 'matplotlib'],
     scripts=['bin/evaluate.py']
 )
