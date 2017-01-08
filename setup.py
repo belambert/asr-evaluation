c5d5f27b4d2970bd1cd9957ce99fdc513b81fd78
Ben Lambert <ben@benjaminlambert.com>
2017-01-08 17:52:57 -0500

    Increment version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='asr_evaluation',
-    version='0.2.3',
+    version='0.2.4',
     author='Ben Lambert',
     author_email='ben@benjaminlambert.com',
     packages=['asr_evaluation'],
