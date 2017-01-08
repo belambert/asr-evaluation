2156b045cafc314ad78a7b9a542b28bfe1303401
Ben Lambert <ben@benjaminlambert.com>
2017-01-08 17:16:30 -0500

    Cleanup script generation

--- setup.py
+++ setup.py
@@ -10,10 +10,9 @@ setup(
     description='Evaluating ASR (automatic speech recognition) hypotheses, i.e. computing word error rate.',
     install_requires=['edit_distance', 'termcolor', 'matplotlib'],
     test_suite='test.test.TestASREvaluation',
-    #scripts=['bin/evaluate.py'],
     entry_points={
         'console_scripts': [
-            'evaluate = asr_evaluation.__main__:main'
+            'evaluate.py = asr_evaluation.__main__:main'
         ]
-    },
+    }
 )
