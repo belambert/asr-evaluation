30061ab2342b8c0e9dcf041a1b43e4c2a3c83dbd
Ben Lambert <ben@benjaminlambert.com>
2017-04-17 20:15:38 -0400

    Convert README.md to RST before uploading to PyPI.

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
 
-version='1.2.1'
+version='1.2.2'
 
 setup(
     name='asr_evaluation',
@@ -10,6 +15,7 @@ setup(
     packages=['asr_evaluation'],
     license='LICENSE.txt',
     description='Evaluating ASR (automatic speech recognition) hypotheses, i.e. computing word error rate.',
+    long_description=long_description,
     install_requires=['edit_distance', 'termcolor'],
     test_suite='test.test.TestASREvaluation',
     entry_points={
